a0f6aa15ad742eea7adb60adbe8b9fac64a50f11
Mfon Ukobo <mfon.ukobo@maxfront.com>
2022-01-23 01:01:23 +0100

    Add test project files

---
+++ Tests/Meetings/CreateMeeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace Tests.Meetings
+{
+	public class CreateMeeting
+	{
+		[Fact]
+		public void TestForParameters()
+		{
+
+		}
+	}
+}
